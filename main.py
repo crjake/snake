95206f62642afe89e315aeb6811bb2a7efc26a33
Charles Jake Dickie <jjd19@ic.ac.uk>
2021-05-30 22:11:31 +0100

    Added growth, food, collision with self and walls, gameover condition.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from snake import Snake
+from snake import Food
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -8,14 +9,20 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Snake')
 
 white = (255,255,255)
+black = (0,0,0)
 
-snake = Snake(400, 300)
+snake = Snake(440, 300)
 
 game_over = False
 
 snake.grow(420, 300)
-snake.grow(440, 300)
+snake.grow(400, 300)
 
+# snake.printSnake()
+
+score = 0
+
+font = pygame.font.SysFont(None, 48)
 
 def readKey(key):
     change_to = ''
@@ -29,11 +36,15 @@ def readKey(key):
         change_to = 'RIGHT'
     return change_to
 
+food = None
 
 while not game_over:
     change_to = snake.direction
     for event in pygame.event.get():
-        game_over = event.type == pygame.QUIT;
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        # game_over = event.type == pygame.QUIT
         if event.type == pygame.KEYDOWN:
             change_to = readKey(event.key)
 
@@ -41,17 +52,58 @@ while not game_over:
 
     screen.fill(white)
 
+    if food == None:
+        food = Food.generateFood(snake)
+
+    if snake.eat(food):
+        score += 1
+        food = None
+
+    if not food == None:
+        food.draw(screen)
+
+
     snake.move(change_to)
+
+    # print(snake.collidesintoitself())
+
+    # snake.printSnake()
+
+    game_over = snake.outofbounds(0, 800, 0, 600) or snake.collidesintoitself()
     snake.draw(screen)
 
-    msElapsed = clock.tick(10)
+    # img = font.render(str(score), True, black)
+    # screen.blit(img, (400, 50))
+
+    scoreText = font.render(str(score), True, black)
+    scoreRect = scoreText.get_rect(center=(400, 50))
+    screen.blit(scoreText, scoreRect)
+
+    msElapsed = clock.tick(12)
 
 
     pygame.display.update()
 
+i = 0
+while True:
+    if i % 2 == 0:
+        color = black
+        screen.fill(white)
+    else:
+        color = white
+        screen.fill(black)
+    scoreText = font.render(str(score), True, color)
+    scoreRect = scoreText.get_rect(center=(400,50))
+    screen.blit(scoreText, scoreRect)
 
-
-
+    gameoverText = font.render("GAMEOVER", True, color)
+    gameoverRect = gameoverText.get_rect(center=(800 / 2, 600 / 2))
+    screen.blit(gameoverText, gameoverRect)
+    pygame.display.update()
+    msElapsed = clock.tick(8)
+    i += 1
+    if i > 5:
+        break
 
 
 pygame.quit()
--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Snake:
     color = (0, 255, 0)
@@ -13,6 +14,30 @@ class Snake:
 
     def grow(self, x, y):
         self.cells.insert(0, Cell(x,y))
+
+    def eat(self, food):
+        head = self.cells[0]
+        if food != None and head.x == food.x and head.y == food.y:
+            self.grow(food.x, food.y)
+            return True
+        return False
+
+    def outofbounds(self, x1, x2, y1, y2):
+        head = self.cells[0]
+        return head.x < x1 or head.x > x2 or head.y < y1 or head.y > y2
+
+    def collides(self, x, y, include_head):
+        for cell in self.cells:
+            if (not include_head) and (cell == self.cells[0]):
+                continue
+            if cell.x == x and cell.y == y:
+                return True
+        return False
+
+
+    def collidesintoitself(self):
+        head = self.cells[0]
+        return self.collides(head.x, head.y, False)
 
     def move(self, new_direction):
         if new_direction == 'UP' and self.direction != 'DOWN':
@@ -47,13 +72,10 @@ class Snake:
             previousX = tempX
             previousY = tempY
 
-
-
-
-
-
-
-
+    def printSnake(self):
+        for cell in self.cells:
+            print("(" + str(cell.x) + ", " + str(cell.y) + ")", end='')
+        print("")
 
 class Cell:
     width = 20
@@ -66,8 +88,6 @@ class Cell:
     def draw(self, screen):
         pygame.draw.rect(screen, Snake.color, (self.x, self.y, Cell.width, Cell.height), 0)
 
-
-
 class Food:
     color = (255, 0, 0)
     def __init__(self, x, y):
@@ -76,4 +96,14 @@ class Food:
 
     def draw(self, screen):
         pygame.draw.rect(screen, Food.color, (self.x, self.y, Cell.width, Cell.height), 0)
+
+
+    @classmethod
+    def generateFood(cls, snake):
+        while True:
+            foodX = round(random.randint(20, 780) / 20) * 20
+            foodY = round(random.randint(20, 580) / 20) * 20
+            if(not snake.collides(foodX, foodY, True)):
+                break
+        return Food(foodX, foodY)
 
